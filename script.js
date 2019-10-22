217dc93700a09578349b9977c0b7c7d9db8040dd
aaron <aaronhalstonlee@gmail.com>
2019-10-22 07:15:27 -0700

    creating game logic

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-computerPlay(){
-    play = ['Rock', 'Paper', 'Scissors']
+function computerPlay(){
+    play = ['rock', 'paper', 'scissors']
     return play[Math.round(Math.random()*3)]
 }
 
-#something
+function playRound(playerSelection, computerSelection){
+    player = playerSelection.toLowerCase();
+    if (player=='rock'&&computerSelection=='paper'){
+        return 'You lose! Paper beats Rock!'
+    } else if(player=='paper'&&computerSelection=='scissors'){
+        return 'You lose! Scissors beats Paper'
+    } else if ()
+}
+
+const playerSelection = 'paper';
+const computerSelection = computerPlay();
