a6345dd2a3afaa21fb6c7655b714a86231ed1a4c
aaron <aaronhalstonlee@gmail.com>
2019-10-23 06:06:25 -0700

    game now playable in console

--- script.js
+++ script.js
@@ -1,16 +1,75 @@
+var playerScore;
+var compScore;
+
 function computerPlay(){
     play = ['rock', 'paper', 'scissors']
     return play[Math.round(Math.random()*3)]
 }
 
-function playRound(playerSelection, computerSelection){
-    player = playerSelection.toLowerCase();
-    if (player=='rock'&&computerSelection=='paper'){
-        return 'You lose! Paper beats Rock!'
-    } else if(player=='paper'&&computerSelection=='scissors'){
-        return 'You lose! Scissors beats Paper'
-    } else if ()
+function playerPoint(){
+    playerScore+=1;
+    console.log(`your points: ${playerScore}`, `computer points: ${compScore}`);
 }
 
-const playerSelection = 'paper';
-const computerSelection = computerPlay();
+function compPoint(){
+    compScore+=1;
+    console.log(`your points: ${playerScore}`, `computer points: ${compScore}`);
+}
+
+function playRound(playerSelection, computerSelection, playerScore, compScore){
+    if (playerSelection == 'rock'){
+        switch(computerSelection){
+            case 'paper':
+                console.log("Paper beats your Rock")
+                compPoint();
+                break;
+            case 'scissors':
+                console.log("Your Rock beats Scissors");
+                playerPoint();
+                break
+            default: 
+                console.log("Tie!");
+        }
+    } else if (playerSelection == 'paper'){
+        switch(computerSelection){
+            case 'rock':
+                console.log("Your Paper beats Rock");
+                playerPoint();
+                break;
+            case 'scissors':
+                console.log("Scissors beats your Paper");
+                compPoint();
+                break;
+            default:
+                console.log("Tie!")
+        }
+    } else if (playerSelection == 'scissors'){
+        switch(computerSelection){
+            case 'rock':
+                console.log("Rock beats your Scissors");
+                compPoint();
+                break;
+            case 'paper':
+                console.log("Your Scissors beats Paper");
+                playerPoint();
+                break;
+            default:
+                console.log("Tie!");
+        }
+    }
+}
+
+function game(int){
+    playerScore=0
+    compScore=0
+    while(int>0){
+        let playerSelection = window.prompt('rock, paper or scissors?', 'paper').toLowerCase();
+        let computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection, playerScore, compScore);
+        int--;
+    }
+    return playerScore>compScore ? "You win!" : "You lose!";
+
+}
+
+console.log(game(6));
